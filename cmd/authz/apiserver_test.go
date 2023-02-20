e04d3f6ecef97341d1a96e52bee53445050a532b
Mark McLaughlin <mmclaugh@redhat.com>
2023-02-20 16:28:00 +0000

    Add CheckPermission endpoint.

--- cmd/authz/apiserver_test.go
+++ cmd/authz/apiserver_test.go
@@ -2,9 +2,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/authzed/authzed-go/proto/authzed/api/v1"
 )
 
 func TestHelloworldService(t *testing.T) {
@@ -23,5 +27,24 @@ func TestHelloworldService(t *testing.T) {
 	want := "Hello, AuthZ!"
 	if w.Body.String() != want {
 		t.Errorf("Response body error: got %v, want %v", w.Body.String(), want)
+	}
+}
+
+func TestCheckPermission(t *testing.T) {
+	cpr := &v1.CheckPermissionRequest{}
+	reqCpr, err := json.Marshal(cpr)
+	if err != nil {
+		t.Errorf("CheckPermissionRequest could not be marshalled.")
+	}
+
+	r := httptest.NewRequest("POST", "/CheckPermission", bytes.NewReader(reqCpr))
+	w := httptest.NewRecorder()
+	h := http.HandlerFunc(CheckPermission)
+
+	h.ServeHTTP(w, r)
+
+	// check HTTP response status code
+	if s := w.Code; s != http.StatusOK {
+		t.Errorf(" Response code error: got %v, want %v", s, http.StatusOK)
 	}
 }
