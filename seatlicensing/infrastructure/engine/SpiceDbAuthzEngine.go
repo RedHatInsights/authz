f6412b83b7661680b9b2285edc5cd19ba11c9659
Dominik Guhr <dguhr@redhat.com>
2023-02-22 15:30:40 +0100

    also change in spiceDbInfra

--- seatlicensing/infrastructure/engine/SpiceDbAuthzEngine.go
+++ seatlicensing/infrastructure/engine/SpiceDbAuthzEngine.go
@@ -1,10 +1,10 @@
 package engine
 
-import "authz/app"
+import "authz/seatlicensing/domain/model"
 
 type SpiceDbAuthzEngine struct{}
 
-func (s SpiceDbAuthzEngine) CheckAccess(principal app.Principal, operation string, resource app.Resource) (bool, error) {
+func (s SpiceDbAuthzEngine) CheckAccess(principal model.Principal, operation string, resource model.Resource) (bool, error) {
 	//TODO: call spiceDB check using their api client
 	return true, nil
 }
