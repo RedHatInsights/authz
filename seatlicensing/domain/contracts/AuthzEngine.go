d62fcbc995a292ec3a7ee0698ba59ae737a28608
Dominik Guhr <dguhr@redhat.com>
2023-02-22 15:24:53 +0100

    Add TODO comment. testfailures etc are expected, this doesn't build without being based on wills branch

--- seatlicensing/domain/contracts/AuthzEngine.go
+++ seatlicensing/domain/contracts/AuthzEngine.go
@@ -2,6 +2,7 @@ package contracts
 
 import "authz/app"
 
+//TODO: Add these from wills draft.
 type AuthzEngine interface {
 	CheckAccess(principal app.Principal, operation string, resource app.Resource) (bool, error)
 }
