4425e07a85355f8b7fd2f1ad65c9463b60ad0e35
William Scalf <wscalf@gmail.com>
2023-02-27 13:54:25 -0500

    Incorporated logging and TLS from upstream

--- host/Web.go
+++ host/Web.go
@@ -6,8 +6,11 @@ import (
 	"authz/app/controllers"
 	"encoding/json"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
+
+	"github.com/golang/glog"
 )
 
 type Web struct {
@@ -16,7 +19,17 @@ type Web struct {
 
 func (web Web) Host(wait *sync.WaitGroup) {
 	http.HandleFunc("/CheckPermission", web.checkPermission)
-	http.ListenAndServe(":8080", nil)
+
+	if _, err := os.Stat("/etc/tls/tls.crt"); err == nil {
+		if _, err := os.Stat("/etc/tls/tls.key"); err == nil { //Cert and key exisits start server in HTTPS mode
+			glog.Info("TLS cert and Key found  - Starting server in secure HTTPs mode")
+
+			_ = http.ListenAndServeTLS(":8443", "/etc/tls/tls.crt", "/etc/tls/tls.key", nil)
+		}
+	} else { // For all cases of error - we start a plain HTTP server
+		glog.Info("TLS cert or Key not found  - Starting server in unsercure plain HTTP mode")
+		_ = http.ListenAndServe(":8080", nil)
+	}
 
 	wait.Done()
 }
@@ -26,6 +39,7 @@ func (web Web) checkPermission(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&webReq)
 	if err != nil {
+		glog.Errorf("Error decoding payload: %s", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -46,13 +60,18 @@ func (web Web) checkPermission(w http.ResponseWriter, r *http.Request) {
 	result, err := action.Check(req)
 
 	if err != nil {
+		glog.Errorf("Error processing request: %s", err)
 		http.Error(w, err.Error(), http.StatusForbidden)
 		return
 	}
 
 	w.Header().Add("Content-Type", "text/plain")
 	w.WriteHeader(200)
-	w.Write([]byte(strconv.FormatBool(result)))
+	_, err = w.Write([]byte(strconv.FormatBool(result)))
+
+	if err != nil {
+		glog.Errorf("Error sending response: %s", err)
+	}
 }
 
 func NewWeb(services Services) Web {
--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"authz/host"
 	"authz/host/impl"
+	"flag"
 	"sync"
+
+	"github.com/golang/glog"
 )
 
 func main() {
+
+	//TODO: factor all this out behind a logging abstraction
+	// This is needed to make `glog` believe that the flags have already been parsed, otherwise
+	// every log messages is prefixed by an error message stating the flags haven't been
+	// parsed.
+	_ = flag.CommandLine.Parse([]string{})
+
+	//pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	// Always log to stderr by default
+	if err := flag.Set("logtostderr", "true"); err != nil {
+		glog.Infof("Unable to set logtostderr to true")
+	}
+
 	services := host.Services{Store: impl.StubAuthzStore{Data: map[string]bool{
 		"token": true,
 		"alice": true,
