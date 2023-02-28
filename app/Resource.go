191fa5045f4bd1b6cc3d0716ad1354452c2606d7
Dominik Guhr <dguhr@redhat.com>
2023-02-28 04:20:43 +0100

    fix linter

--- app/Resource.go
+++ app/Resource.go
@@ -1,6 +1,6 @@
 package app
 
-//A Resource is a securable asset Principals may or may not have authority over
+// A Resource is a securable asset Principals may or may not have authority over
 type Resource struct {
 	//The Type is the kind of Resource that it is
 	Type string
