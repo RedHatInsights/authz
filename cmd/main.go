7f0a9fa5dd4b62d56159637c5c8c3a7a58660aab
Dominik Guhr <dguhr@redhat.com>
2023-03-11 00:27:35 +0100

    add config usage and add mitigation for flag logging foo

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,22 @@ package main
 
 import (
 	"authz/app"
+	"flag"
+	"github.com/golang/glog"
 )
 
 // main bootstrapping the current composition of the service
 func main() {
+
+	// Needed to make `glog` believe that the flags have already been parsed, otherwise
+	// every log messages is prefixed by an error message stating the flags haven't been
+	// parsed.
+	_ = flag.CommandLine.Parse([]string{})
+
+	// Always log to stderr by default
+	if err := flag.Set("logtostderr", "true"); err != nil {
+		glog.Infof("Unable to set logtostderr to true")
+	}
+
 	app.Run()
 }
