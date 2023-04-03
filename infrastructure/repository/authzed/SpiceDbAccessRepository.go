c8d1307203b3e35eb4d8884301182a2c08585b7d
William Scalf <wscalf@gmail.com>
2023-04-03 17:35:42 -0400

    Removed TODO comment and renamed variable

--- infrastructure/repository/authzed/SpiceDbAccessRepository.go
+++ infrastructure/repository/authzed/SpiceDbAccessRepository.go
@@ -332,9 +332,9 @@ func (s *SpiceDbAccessRepository) NewConnection(spiceDbEndpoint string, token st
 
 	if !useTLS {
 		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	} else { // Use TLS - but for now SKIP CA verification
-		skipCA, _ := grpcutil.WithSystemCerts(grpcutil.VerifyCA)
-		opts = append(opts, skipCA)
+	} else {
+		tlsConfig, _ := grpcutil.WithSystemCerts(grpcutil.VerifyCA)
+		opts = append(opts, tlsConfig)
 	}
 
 	client, err := authzed.NewClient(
