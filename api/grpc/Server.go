c5fe6f67ccaca836e9c59e395daa6fa8480812e9
Mark McLaughlin <mmclaugh@redhat.com>
2023-05-16 16:54:52 +0100

    Add TODO to grpc/Server.go to flag new config functionality coming soon

--- api/grpc/Server.go
+++ api/grpc/Server.go
@@ -155,7 +155,7 @@ func (s *Server) Serve(wait *sync.WaitGroup) error {
 			s.ServerConfig.GrpcPort)
 	}
 
-	authConfigs := []api.AuthConfig{s.ServerConfig.AuthConfig}
+	authConfigs := []api.AuthConfig{s.ServerConfig.AuthConfig} // TODO: wire up with new file-based config functionality
 	authMiddleware, err := interceptor.NewAuthnInterceptor(authConfigs)
 	if err != nil {
 		glog.Fatalf("Error: Not able to reach discovery endpoint to initialize authentication middleware.")
