7547a21eb456398382d6e5121c5ed0f3aee6917a
Dominik Guhr <dguhr@redhat.com>
2023-05-22 16:51:11 +0200

    Fix for the panic when no authconfig is given
    Introduces PassthroughInterceptor when all AuthConfigs are set to enabled:false
    
    Co-authored-by: William Scalf <wscalf@gmail.com>
    
    Co-authored-by: Mark McLaughlin <mmclaugh@redhat.com>

---
+++ api/grpc/interceptor/PassthroughAuthnInterceptor.go
@@ -0,0 +1,23 @@
+package interceptor
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+// PassthroughAuthnInterceptor - passthrough AuthN Middleware for usage in local dev use cases where authN does not matter.
+type PassthroughAuthnInterceptor struct{}
+
+// NewPassthroughAuthnInterceptor creates a new PassthroughAuthnInterceptor
+func NewPassthroughAuthnInterceptor() *PassthroughAuthnInterceptor {
+	return &PassthroughAuthnInterceptor{}
+}
+
+// Unary impl of the Unary passthrough interceptor, returning a static value in the context..
+func (authnInterceptor *PassthroughAuthnInterceptor) Unary() grpc.ServerOption {
+	return grpc.UnaryInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+		return handler(context.WithValue(ctx, RequestorContextKey, "static-subject"), req)
+	})
+}
