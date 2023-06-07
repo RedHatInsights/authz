c98c17967dded3d9e58bf644c1e08842ab5c0ef5
William Scalf <wscalf@gmail.com>
2023-06-06 23:17:25 -0400

    Makes it possible to assert access to a service

--- bootstrap/app_test.go
+++ bootstrap/app_test.go
@@ -474,6 +474,15 @@ func TestGrantedLicenseAllowsUse(t *testing.T) {
 	assertJSONResponse(t, resp, 200, `{"result": %t, "description": ""}`, true)
 }
 
+func TestGrantedLicenseGivesAccessToServices(t *testing.T) {
+	setupService()
+	defer teardownService()
+	resp, err := http.DefaultClient.Do(post("/v1alpha/check", "system",
+		`{"subject": "u1", "operation": "access", "resourcetype": "service", "resourceid": "smarts"}`))
+	assert.NoError(t, err)
+	assertJSONResponse(t, resp, 200, `{"result": %t, "description": ""}`, true)
+}
+
 func TestGrantedLicenseAffectsCountsAndDetails(t *testing.T) {
 	setupService(nil)
 	defer teardownService()
