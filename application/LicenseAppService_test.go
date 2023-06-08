31397dd8fca58d25b6b3ed50dfac7d1be9ab3b72
Dominik Guhr <dguhr@redhat.com>
2023-06-08 09:52:03 +0200

    Added test: same org and service added twice does result in error and does not change license data in spiceDB

--- application/LicenseAppService_test.go
+++ application/LicenseAppService_test.go
@@ -44,6 +44,49 @@ func TestOrgEnablement(t *testing.T) {
 	assert.Equal(t, 20, len(assignable))
 }
 
+func TestSameOrgAndServiceAddedTwiceNotPossible(t *testing.T) {
+	//Given
+	service := createService()
+	evt := OrgEntitledEvent{
+		OrgID:     "o2",
+		ServiceID: "smarts",
+		MaxSeats:  2,
+	}
+
+	evt2 := OrgEntitledEvent{
+		OrgID:     "o2",
+		ServiceID: "smarts",
+		MaxSeats:  1,
+	}
+
+	//When
+	err := service.ProcessOrgEntitledEvent(evt)
+	assert.NoError(t, err)
+	err = service.ProcessOrgEntitledEvent(evt2)
+	assert.Error(t, err)
+
+	spicedbContainer.WaitForQuantizationInterval()
+
+	limit, available, err := service.GetSeatAssignmentCounts(GetSeatAssignmentCountsRequest{
+		Requestor: "system",
+		OrgID:     evt.OrgID,
+		ServiceID: evt.ServiceID,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, evt.MaxSeats, limit)
+	assert.Equal(t, evt.MaxSeats, available) //None in use
+
+	assignable, err := service.GetSeatAssignments(GetSeatAssignmentRequest{
+		Requestor:    "system",
+		OrgID:        evt.OrgID,
+		ServiceID:    evt.ServiceID,
+		IncludeUsers: false,
+		Assigned:     false,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 20, len(assignable))
+}
+
 func createService() *LicenseAppService {
 	spiceDbRepo, err := spicedbContainer.CreateClient()
 	if err != nil {
