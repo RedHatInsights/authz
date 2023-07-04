57c7836fdb88301aee752bf9d0a424e80cc63e2f
Dominik Guhr <dguhr@redhat.com>
2023-07-04 10:29:31 +0200

    update image in tests to v1.22.2 - tests green

--- bootstrap/serviceconfig/ServiceConfig.go
+++ bootstrap/serviceconfig/ServiceConfig.go
@@ -9,7 +9,7 @@ const (
 	// SpicedbImage is the image used for containerized spiceDB in tests
 	SpicedbImage = "authzed/spicedb"
 	// SpicedbVersion is the image version used for containerized spiceDB in tests
-	SpicedbVersion = "v1.21.0"
+	SpicedbVersion = "v1.22.2"
 )
 
 // ServiceConfig contains all server-related configuration.
