9cb5da4d4b9d478a8c14fa59be92c210ec2e8e46
Mark McLaughlin <mmclaugh@redhat.com>
2023-07-06 11:18:13 +0100

    Use oauth client grant to supply access token for check calls.
    Co-authored-by: Dominik Guhr <dguhr@redhat.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,14 @@ idpDiscoveryUri=$2
 orgId=$3
 userId=$4
 maxSeats=$5
+ctoken_client_id=$6
+client_secret=$7
+client_scope=$8
 
 token=""
+clienttoken=""
+
 client_id=cloud-services
-scopes=openid
 port=8000
 
 function login() {
@@ -15,7 +19,7 @@ function login() {
   authzEndpoint=$(echo "$metadata" | jq -r ".authorization_endpoint")
   tokenEndpoint=$(echo "$metadata" | jq -r ".token_endpoint")
 
-  echo "Please open this url in a browser: $authzEndpoint?response_type=code&client_id=$client_id&redirect_uri=http://127.0.0.1:$port/&scope=$scopes" #https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+  echo "Please open this url in a browser: $authzEndpoint?response_type=code&client_id=$client_id&redirect_uri=http://127.0.0.1:$port/&scope=$client_scope" #https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
 
   echo "Listening for response from identity provider.."
   #1 the response that should be sent to the browser gets piped into netcat. Note Connection: close is important because netcat won't exit until the client disconnects, and some browsers will keep a connection open for pooling purposes unless told not to.
@@ -30,6 +34,14 @@ function login() {
   token_response=$(curl -s -X POST -d "grant_type=authorization_code&client_id=$client_id&code=$authzcode&redirect_uri=http://127.0.0.1:$port/" $tokenEndpoint)
   token=$(echo "$token_response" | jq -r '.access_token') #Parse the access token out of the response and set it to a global: https://openid.net/specs/openid-connect-core-1_0.html#TokenResponse
 }
+
+function client_grant() {
+  metadata=$(curl -s "$idpDiscoveryUri")
+  tokenEndpoint=$(echo "$metadata" | jq -r ".token_endpoint")
+  ctoken_response=$(curl -s -X POST -d "grant_type=client_credentials&client_id=$ctoken_client_id&client_secret=$client_secret&scope=$client_scope" $tokenEndpoint)
+  clienttoken=$(echo "$ctoken_response" | jq -r '.access_token')
+}
+
 
 function fail() {
     echo "$1"
@@ -83,7 +95,7 @@ sleep 5
 
 msg="Checking access for $userId (should succeed)"
 echo "Test: $msg"
-ret=`( curl --silent --fail -X POST $baseUri/v1alpha/check -H "Origin: http://smoketest.test" -H "Content-Type: application/json" -H "Authorization:Bearer $token" -d '{"subject": "'$userId'", "operation": "access", "resourcetype": "license", "resourceid": "'$orgId'/smarts"}' || fail "Failed request: $msg" ) | jq ".result"`
+ret=`( curl --silent --fail -X POST $baseUri/v1alpha/check -H "Origin: http://smoketest.test" -H "Content-Type: application/json" -H "Authorization:Bearer $clienttoken" -d '{"subject": "'$userId'", "operation": "access", "resourcetype": "license", "resourceid": "'$orgId'/smarts"}' || fail "Failed request: $msg" ) | jq ".result"`
 assert "$ret = true" "$msg"
 
 msg="Checking if $userId is included in the list of assigned users"
@@ -107,7 +119,7 @@ assert "$previousAvailable -lt $newAvailable" "$msg"
 
 msg="Checking access for $userId again (should return false)"
 echo "Test: $msg"
-ret=`( curl --silent --fail -X POST $baseUri/v1alpha/check -H "Origin: http://smoketest.test" -H "Content-Type: application/json" -H "Authorization:Bearer $token" -d '{"subject": "'$userId'", "operation": "access", "resourcetype": "license", "resourceid": "'$orgId'/smarts"}' || fail "Failed request: $msg" ) | jq ".result"`
+ret=`( curl --silent --fail -X POST $baseUri/v1alpha/check -H "Origin: http://smoketest.test" -H "Content-Type: application/json" -H "Authorization:Bearer $clienttoken" -d '{"subject": "'$userId'", "operation": "access", "resourcetype": "license", "resourceid": "'$orgId'/smarts"}' || fail "Failed request: $msg" ) | jq ".result"`
 assert "$ret = false" "$msg"
 
 echo "PASSED ALL TESTS"
