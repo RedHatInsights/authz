2c0ea4ca817085e0a178ce16f12c3d470c200386
William Scalf <wscalf@gmail.com>
2023-07-24 18:07:34 -0400

    Added tests to specific validation rules

--- application/Validation.go
+++ application/Validation.go
@@ -60,14 +60,29 @@ var spiceDbPermissionPattern = regexp.MustCompile(`^([a-z][a-z0-9_]{1,62}[a-z0-9
 var spiceDbTypePattern = regexp.MustCompile(`^([a-z][a-z0-9_]{1,61}[a-z0-9]/)?[a-z][a-z0-9_]{1,62}[a-z0-9]$`)
 
 func validateSpiceDbID(fl validator.FieldLevel) bool {
-	return spiceDbIDPattern.MatchString(fl.Field().String())
+	val := fl.Field().String()
+	if val == "" {
+		return true
+	}
+
+	return spiceDbIDPattern.MatchString(val)
 }
 
 func validateSpiceDbPermission(fl validator.FieldLevel) bool {
+	val := fl.Field().String()
+	if val == "" {
+		return true
+	}
+
 	return spiceDbPermissionPattern.MatchString(fl.Field().String())
 }
 
 func validateSpiceDbType(fl validator.FieldLevel) bool {
+	val := fl.Field().String()
+	if val == "" {
+		return true
+	}
+
 	return spiceDbTypePattern.MatchString(fl.Field().String())
 }
 
@@ -79,11 +94,16 @@ func validateIn(fl validator.FieldLevel) bool {
 	if !ok {
 		config = make(map[string]bool)
 
-		for _, val := range strings.Split(param, ",") {
+		for _, val := range strings.Split(param, "+") {
 			config[val] = true
 		}
 
 		inConfigCache[param] = config
+	}
+
+	val := fl.Field().String()
+	if val == "" {
+		return true
 	}
 
 	return config[fl.Field().String()]
